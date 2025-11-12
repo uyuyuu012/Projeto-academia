1c6db08b3d9552115f6bdb3ced68ecf7f604e4dd
Guilherme <guitama.games@gmail.com>
2025-11-12 15:22:30 -0300

    Coloando o node.js para o banco de dados

--- src/SQL/projetoacademia.sql
+++ src/SQL/projetoacademia.sql
@@ -1,8 +1,15 @@
-create database dbescola;
+create database if not exists dbescola;
 use dbescola;
-create table texte(
+
+create table if not exists texte(
     ID int primary key,
     Nome text not null,
     UserStatus text not null    
 );
 
+delimiter $$
+create procedure inserirCliente (in Nome varchar(100))
+begin
+    insert into texte (nome slk)
+end
+$$
